f27c9bac86da33ec29774adbb2a41e087ee62bc9
Myocardial-infarction-Jerry <2428746252@qq.com>
2023-11-27 18:21:52 +0800

    Rebuild Final-Project

--- Final-Project/includes/color.h
+++
@@ -1,9 +0,0 @@
-#ifndef COLOR_H
-#define COLOR_H
-
-#include <iostream>
-#include "vec3.h"
-
-std::ostream &operator<<(std::ostream &out, const color &c);
-
-#endif
--- Final-Project/src/color.cpp
+++
@@ -1,5 +0,0 @@
-#include "color.h"
-
-std::ostream &operator<<(std::ostream &out, const color &c) {
-    return out << static_cast<int>(255.999 * c[0]) << ' ' << static_cast<int>(255.999 * c[1]) << ' ' << static_cast<int>(255.999 * c[2]) << '\n';
-}
